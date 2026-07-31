bf90bc5656bc2a99c68ecc664cb8a8b4cc237119
starsli <starsli@tencent.com>
2026-07-31 16:54:08 +0800

    sql文件调整

--- model/mysql/account_init.sql
+++ model/mysql/account_init.sql
@@ -103,6 +103,26 @@ CREATE TABLE `t_c2c_bill` (
   INDEX `idx_update_time` (`update_time`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- c2b待转账表
+DROP TABLE IF EXISTS `t_c2b_bill`;
+CREATE TABLE `t_c2b_bill` (
+  `transaction_id` VARCHAR(64) NOT NULL COMMENT '交易ID',
+  `uid` BIGINT NOT NULL COMMENT '用户UID',
+  `user_id` VARCHAR(64) NOT NULL COMMENT '用户ID',
+  `merchant_uid` BIGINT NOT NULL COMMENT '商户UID',
+  `merchant_id` VARCHAR(64) NOT NULL COMMENT '商户ID',
+  `amount` VARCHAR(64) NOT NULL COMMENT '金额',
+  `state` TINYINT NOT NULL COMMENT '单状态',
+  `biz_type` INTEGER NOT NULL COMMENT '业务类型',
+  `desc` VARCHAR(256) NOT NULL COMMENT '转账描述',
+  `pay_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '支付时间',
+  `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',   
+  PRIMARY KEY (`transaction_id`),
+  INDEX `idx_create_time` (`create_time`),
+  INDEX `idx_update_time` (`update_time`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 DROP TABLE IF EXISTS `t_save_bill`;
 CREATE TABLE `t_save_bill` (
   `transaction_id` VARCHAR(64) NOT NULL COMMENT '交易ID',
@@ -127,6 +147,24 @@ CREATE TABLE `t_c2c_pending_transfer` (
   `seller_uid` BIGINT NOT NULL COMMENT '卖家用户UID',
   `buyer_user_id` VARCHAR(64) NOT NULL COMMENT '买家用户ID',
   `seller_user_id` VARCHAR(64) NOT NULL COMMENT '卖家用户ID',
+  `amount` BIGINT NOT NULL COMMENT '金额',
+  `state` TINYINT NOT NULL COMMENT '状态',
+  `desc` VARCHAR(256) NOT NULL COMMENT '转账描述',
+  `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',  
+  PRIMARY KEY (`transaction_id`),
+  INDEX `idx_create_time` (`create_time`),
+  INDEX `idx_update_time` (`update_time`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- c2b待转账表
+DROP TABLE IF EXISTS `t_c2b_pending_transfer`;
+CREATE TABLE `t_c2b_pending_transfer` (
+  `transaction_id` VARCHAR(64) NOT NULL COMMENT '交易ID',
+  `uid` BIGINT NOT NULL COMMENT '用户UID',
+  `user_id` VARCHAR(64) NOT NULL COMMENT '用户ID',
+  `merchant_uid` BIGINT NOT NULL COMMENT '商户UID',
+  `merchant_id` VARCHAR(64) NOT NULL COMMENT '商户ID',
   `amount` BIGINT NOT NULL COMMENT '金额',
   `state` TINYINT NOT NULL COMMENT '状态',
   `desc` VARCHAR(256) NOT NULL COMMENT '转账描述',
