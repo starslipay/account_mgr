012da75331f29c7033e39c503cb6764dac71294e
starsli <starsli@tencent.com>
2026-09-23 19:07:21 +0800

    save

--- internal/xerr/xerr.go
+++ internal/xerr/xerr.go
@@ -4,24 +4,23 @@ package xerr
 // 模块id  30000
 // 错误码 = 模块id + 业务错误码
 var (
-	ModuleId        = int64(455905)
-	ModuleErrorBase = ModuleId * 100
+	ModuleId = int64(455905)
 )
 
 var (
 	// 系统错误 0000-0999
-	ErrCodeServerInternal = ModuleErrorBase + 1
+	ErrCodeServerInternal = int64(455905001)
 
 	// 业务错误码 1000-1999
-	ErrCodeParam                      = ModuleErrorBase + 100
-	ErrCodeBalanceNotEnough           = ModuleErrorBase + 101
-	ErrCodeDB                         = ModuleErrorBase + 102
-	ErrCodeRepeatButInfoNotConsistent = ModuleErrorBase + 103 // 重入,但信息不一致
-	ErrCodeC2CBillNotFound            = ModuleErrorBase + 104 // C2C单据不存在
-	ErrCodeBillStateNotOK             = ModuleErrorBase + 105 // 单据状态不是OK
-	ErrCodeSupplyModeC2BBillNotFound  = ModuleErrorBase + 106 // 补单模式下C2B单据不存在
-	ErrCodeC2BBillStateInvalid        = ModuleErrorBase + 107 // C2B单据状态无效
-	ErrCodeC2BBillStateAlreadyClose   = ModuleErrorBase + 108 // C2B单据状态已关闭
-	ErrCodeC2BBillNotFound            = ModuleErrorBase + 109 // C2B单据不存在
-	ErrCodeC2BBillConflict            = ModuleErrorBase + 110 // C2B单据已存在,插入冲突
+	ErrCodeParam                      = int64(455905100)
+	ErrCodeBalanceNotEnough           = int64(455905101)
+	ErrCodeDB                         = int64(455905102)
+	ErrCodeRepeatButInfoNotConsistent = int64(455905103) // 重入,但信息不一致
+	ErrCodeC2CBillNotFound            = int64(455905104) // C2C单据不存在
+	ErrCodeBillStateNotOK             = int64(455905105) // 单据状态不是OK
+	ErrCodeSupplyModeC2BBillNotFound  = int64(455905106) // 补单模式下C2B单据不存在
+	ErrCodeC2BBillStateInvalid        = int64(455905107) // C2B单据状态无效
+	ErrCodeC2BBillStateAlreadyClose   = int64(455905108) // C2B单据状态已关闭
+	ErrCodeC2BBillNotFound            = int64(455905109) // C2B单据不存在
+	ErrCodeC2BBillConflict            = int64(455905110) // C2B单据已存在,插入冲突
 )
